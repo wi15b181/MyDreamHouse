4312c86d198f806312e129f24c194bd2d6b28efe
alexfuernsinn <alex.fuernsinn@aon.at>
2017-06-21 23:32:12 +0200

    First work

---
+++ Desktop/MyDreamHouseDesktop/SynchronisationLib/WebServiceConnector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SynchronisationLib.SynchronisationService;
+
+namespace SynchronisationLib
+{
+    class WebServiceConnector
+    {
+        SynchronisationWCFClient client = new SynchronisationService.SynchronisationWCFClient();
+
+        public WebServiceConnector()
+        {
+
+        }
+        public SyncDataSet FullSync()
+        {
+            return client.Synchronize("", "");
+        }
+
+        public SyncDataSet PartialSync()
+        {
+            return client.Synchronize("", "");
+        }
+
+        public int Ping()
+        {
+            return client.Ping();
+        }
+
+    }
+}
