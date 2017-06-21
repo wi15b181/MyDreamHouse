4c6086a35132603fd6c46f8ffab1f640cc115d58
Markus <markus.schick@technikum-wien.at>
2017-06-22 01:27:20 +0200

    Daos für alle Attribut-Tabellen + Attachement + Hauspaket Entites für alle oben genannten Tabellen

---
+++ Desktop/MyDreamHouseDesktop/SharedLibrary/Entities/HauspaketAttributWertEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Entities
+{
+    public class HauspaketAttributWertEntity
+    {
+        public int WertId { get; set; }
+
+        public int AttributId { get; set; }
+
+        public string WertText { get; set; }
+
+        public int WertOrdnung { get; set; }
+
+        public bool Archived { get; set; }
+    }
+}
---
+++ Desktop/MyDreamHouseDesktop/SharedLibrary/Entities/HauspaketEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class HauspaketEntity
+    {
+        public int HauspaketId { get; set; }
+
+        public int HerstellerId { get; set; }
+
+        public int BeraterId { get; set; }
+
+        public string Bezeichnung { get; set; }
+
+        public decimal Preis { get; set; }
+
+        public decimal Grundflaeche { get; set; }
+
+        public decimal Wohnflaeche { get; set; }
+
+        public int Stockwerke { get; set; }
+
+        public int BenutzerId { get; set; }
+
+        public bool Archived { get; set; }
+    }
+}
