5c808b78b2287a6f160acbc3f72eb08a07401b5b
Johannes <wi15b039@technikum-wien.at>
2017-06-25 18:44:36 +0200

    DB2-Verbindungsvalue nun unter globaler Vriable $conn von überall aus abrufbar.

--- Backend/MyDreamHouse2/app/controllers/application_controller.rb
+++ Backend/MyDreamHouse2/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
  # def index
   #end
+  $conn = IBM_DB.connect("DRIVER={IBM DB2 ODBC DRIVER};DATABASE=mdh;\
+                       HOSTNAME=wi-gate.technikum-wien.at;PORT=60831;PROTOCOL=TCPIP;\
+                       UID=mydreahmouse;PWD=wi15b;", "", "")
   protect_from_forgery with: :exception
 end
