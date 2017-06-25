3979fdf821a1eb5973759bdb495f31a0bda0b8db
Johannes <wi15b039@technikum-wien.at>
2017-06-25 14:13:15 +0200

    Benutzerrollen verwaltung hinzugefügt. Login unter /welcome/index hinzugefügt.

--- Backend/MyDreamHouse2/app/controllers/benutzerrollen_controller.rb
+++ Backend/MyDreamHouse2/app/controllers/benutzerrollen_controller.rb
@@ -1,5 +1,7 @@
 class BenutzerrollenController < ApplicationController
   def show
-
+    $conn = IBM_DB.connect("DRIVER={IBM DB2 ODBC DRIVER};DATABASE=mdh;\
+                       HOSTNAME=wi-gate.technikum-wien.at;PORT=60831;PROTOCOL=TCPIP;\
+                       UID=mydreahmouse;PWD=wi15b;", "", "")
   end
 end
